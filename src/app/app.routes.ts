2ec09f50be394ffbf3f8874c5a841c18fecdd299
renzito0637d <renzito0637d@gmail.com>
2025-09-09 16:18:09 -0500

    feat: ruta pagina de redes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { RedesComponent } from './pages/redes/redes.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent, pathMatch: 'full' },
   { path: 'nosotros', component: NosotrosComponent },
   { path: 'catalogo', component: CatalogoComponent },
+  { path: 'redes', component: RedesComponent },
   { path: '**', redirectTo: '' }
 ];
