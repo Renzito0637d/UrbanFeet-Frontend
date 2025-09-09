4c27104bc240d8a1a571b13e243a70b3a1a7e369
renzito0637d <renzito0637d@gmail.com>
2025-09-09 15:48:51 -0500

    feat: ruta de pagina catalogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent, pathMatch: 'full' },
   { path: 'nosotros', component: NosotrosComponent },
+  { path: 'catalogo', component: CatalogoComponent },
   { path: '**', redirectTo: '' }
 ];
