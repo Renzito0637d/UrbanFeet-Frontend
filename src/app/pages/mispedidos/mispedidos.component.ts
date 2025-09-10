48411e9784d3c0d9a1a834925085daa19034d349
renzito0637d <renzito0637d@gmail.com>
2025-09-09 23:12:25 -0500

    feat: componente "Mis pedidos" del cliente

---
+++ src/app/pages/mispedidos/mispedidos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mispedidos',
+  imports: [],
+  templateUrl: './mispedidos.component.html',
+  styleUrl: './mispedidos.component.css'
+})
+export class MispedidosComponent {
+
+}
