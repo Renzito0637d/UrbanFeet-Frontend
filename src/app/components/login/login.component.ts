e6b294b7889b256b6b2366f623e57942d4178fb0
renzito0637d <renzito0637d@gmail.com>
2025-11-12 19:27:32 -0500

    feat: login funcional devuelve jwt en cookies en el navegador

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,10 +2,20 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { HeaderComponent } from '../header/header.component';
 import { RegisterComponent } from '../register/register.component';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../models/auth.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -26,5 +36,43 @@ export class LoginComponent {
       enterAnimationDuration,
       exitAnimationDuration,
     });
+  }
+
+  // Inyectamos los servicios que necesitamos
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private http = inject(HttpClient);
+
+  // Creamos el formulario reactivo
+  public loginForm: FormGroup = this.fb.group({
+    // El primer string es el valor por defecto y validadores
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  // El método que se llama al enviar el formulario
+  onSubmit(): void {
+
+    // Creamos el DTO (Request) desde los valores del formulario
+    const credentials: LoginRequest = this.loginForm.value;
+
+    console.log('Llamando al servicio de login con credenciales:', credentials);
+    this.authService.login(credentials).subscribe({
+      // Manejamos el ÉXITO
+      next: (user) => {
+        // El login fue exitoso, 'user' es el objeto que devolvió el backend.
+        // El servicio (AuthService) ya guardó el estado.
+        // Ahora, redirigimos al usuario
+        console.log('Login exitoso:', user);
+        this.router.navigate(['/']);
+      },
+      // Manejamos el ERROR
+      error: (err) => {
+        // El login falló (ej. 401 Credenciales incorrectas)
+        console.error('Error en el login:', err);
+      }
+    });
+
   }
 }
