5479be970b084e58b174bf38a4d443558893d57a
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 01:24:13 -0500

    feat: componente zapatillas

---
+++ src/app/admin/pages/zapatillas/zapatillas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zapatillas',
+  imports: [],
+  templateUrl: './zapatillas.component.html',
+  styleUrl: './zapatillas.component.css'
+})
+export class ZapatillasComponent {
+
+}
