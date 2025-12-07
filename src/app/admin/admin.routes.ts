7e4bdc1e25011e59754702108810a53a22475dfb
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 01:23:43 -0500

    feat: nuevos rutas para zapatillas y zapatilla variacion

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -8,6 +8,8 @@ import { PedidosComponent } from './pages/pedidos/pedidos.component';
 import { VentasComponent } from './pages/ventas/ventas.component';
 import { authGuard } from '../guards/auth.guard';
 import { ReclamacionesComponent } from './pages/reclamaciones/reclamaciones.component';
+import { ZapatillasComponent } from './pages/zapatillas/zapatillas.component';
+import { VariacionesComponent } from './pages/variaciones/variaciones.component';
 
 export const ADMIN_ROUTES: Routes = [
     {
@@ -16,7 +18,8 @@ export const ADMIN_ROUTES: Routes = [
         children: [
             { path: '', component: InicioComponent, pathMatch: 'full' },
             { path: 'usuarios', component: UsuariosComponent, canActivate: [authGuard], data: { roles: ['ADMIN'] } },
-            { path: 'catalogo', component: CatalogoComponent, canActivate: [authGuard], data: { roles: ['INVENTARIO', 'ADMIN'] } },
+            { path: 'zapatillas', component: ZapatillasComponent, canActivate: [authGuard], data: { roles: ['INVENTARIO', 'ADMIN'] } },
+            { path: 'variaciones', component: VariacionesComponent, canActivate: [authGuard], data: { roles: ['INVENTARIO', 'ADMIN'] } },
             { path: 'pedidos', component: PedidosComponent, canActivate: [authGuard], data: { roles: ['PEDIDOS', 'ADMIN'] } },
             { path: 'ventas', component: VentasComponent, canActivate: [authGuard], data: { roles: ['VENTAS', 'ADMIN'] } },
             { path: 'reclamaciones', component: ReclamacionesComponent },
