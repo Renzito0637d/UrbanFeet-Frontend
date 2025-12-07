9d4d61fdfcce2e96d4c9e224a0fde181abcaaa7a
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 01:24:04 -0500

    feat: componente variacion

---
+++ src/app/admin/pages/variaciones/variaciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-variaciones',
+  imports: [],
+  templateUrl: './variaciones.component.html',
+  styleUrl: './variaciones.component.css'
+})
+export class VariacionesComponent {
+
+}
