7f566ed5aa8d127038e3fba995de51d2f48ff9e0
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 19:19:20 -0500

    feat: servicios de zapatillas y variaciones

---
+++ src/app/services/variaciones.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Variacion } from '../models/variaciones.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VariacionesService {
+  private http = inject(HttpClient);
+  // Base del controlador que me pasaste
+  private baseUrl = '/zapatilla-variacion';
+
+  // GET: /zapatilla-variacion/zapatillas/{id}
+  getByZapatillaId(id: number): Observable<Variacion[]> {
+    return this.http.get<Variacion[]>(`${this.baseUrl}/zapatillas/${id}`);
+  }
+
+  // POST: /zapatilla-variacion/zapatilla/{id}
+  create(zapatillaId: number, variacion: Variacion): Observable<Variacion> {
+    return this.http.post<Variacion>(`${this.baseUrl}/zapatilla/${zapatillaId}`, variacion);
+  }
+
+  update(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, data); // Asumiendo que tu endpoint PUT es /zapatilla-variacion/{id}
+  }
+
+  // (Opcional) DELETE si lo implementas luego
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
