010219e8061df0e638e0b490f72b486f4339d6bd
Angel <luang1007@gmail.com>
2025-12-08 19:43:47 -0500

    Feature: CRUD de sugerencias front

--- src/app/pages/contactanos/contactanos.component.ts
+++ src/app/pages/contactanos/contactanos.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SugerenciaService } from '../../services/sugerencia.service';
 
 @Component({
   selector: 'app-contactanos',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contactanos.component.html',
   styleUrl: './contactanos.component.css'
 })
 export class ContactanosComponent {
 
+  asunto: string = "";
+  mensaje: string = "";
+
+  constructor(private sugerenciaService: SugerenciaService) {}
+
+  enviarFormulario() {
+    if (!this.asunto || !this.mensaje) {
+      alert("Por favor complete el asunto y mensaje.");
+      return;
+    }
+
+    this.sugerenciaService.crearSugerencia({
+      asunto: this.asunto,
+      mensaje: this.mensaje
+    }).subscribe({
+      next: () => {
+        alert("Mensaje enviado correctamente");
+        this.asunto = "";
+        this.mensaje = "";
+      },
+      error: () => {
+        alert("Ocurrió un error al enviar el mensaje");
+      }
+    });
+  }
 }
