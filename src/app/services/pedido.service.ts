63fa85aae2036d1938141216a2fdf880817e7dd6
Renzito0637d <renzito0637d@gmail.com>
2025-12-09 15:06:56 -0500

    feat: servicios para los pedidos

---
+++ src/app/services/pedido.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PedidoRequest, PedidoResponse } from '../models/pedido.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+  private http = inject(HttpClient);
+  private baseUrl = '/pedidos';
+
+  crearPedido(request: PedidoRequest): Observable<PedidoResponse> {
+    return this.http.post<PedidoResponse>(this.baseUrl, request);
+  }
+
+  listarMisPedidos(): Observable<PedidoResponse[]> {
+    return this.http.get<PedidoResponse[]>(this.baseUrl);
+  }
+}
