1aad4507ab28745658589b3d87c83c54fcc7452d
Renzito0637d <renzito0637d@gmail.com>
2025-12-09 23:37:30 -0500

    feat: servicios para venta y nuevos para pedidos

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -22,4 +22,23 @@ export class PedidoService {
     // PATCH suele requerir un cuerpo, aunque sea vacío
     return this.http.patch<void>(`${this.baseUrl}/${id}/cancelar`, {});
   }
+
+  getAllAdmin(): Observable<PedidoResponse[]> {
+    return this.http.get<PedidoResponse[]>(`${this.baseUrl}/all`);
+  }
+
+  // Actualizar estado (PATCH)
+  updateStatus(id: number, nuevoEstado: string): Observable<void> {
+    return this.http.patch<void>(`${this.baseUrl}/${id}/estado`, { estado: nuevoEstado });
+  }
+
+  // Actualizar pedido completo (Admin editando cantidades/dirección)
+  updatePedido(id: number, data: any): Observable<PedidoResponse> {
+    // CAMBIO AQUÍ: Apuntamos a /admin/{id}
+    return this.http.put<PedidoResponse>(`${this.baseUrl}/admin/${id}`, data);
+  }
+
+  deletePedido(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
