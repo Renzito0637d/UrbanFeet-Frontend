c0625fcec3a15f894655617d0e50417d51300fa0
Renzito0637d <renzito0637d@gmail.com>
2025-12-11 01:21:48 -0500

    feat: model de reclamo

---
+++ src/app/models/reclamo.model.ts
@@ -0,0 +1,28 @@
+export interface DatosUsuarioReclamo {
+    nombre: string;
+    documento: string;
+    domicilio: string;
+    correo: string;
+    telefono: string;
+}
+
+export interface ReclamacionRequest {
+    producto: string;
+    montoReclamado: number;
+    tipoMensaje: string;
+    detalleReclamo: string;
+    solucionPropuesta: string;
+    direccion: string;
+}
+
+export interface ReclamacionResponse {
+    id: number;
+    producto: string;
+    montoReclamado: number;
+    tipoMensaje: string;
+    detalleReclamo: string;
+    solucionPropuesta: string;
+    fechaRegistro: string;
+    estado: string;
+    direccion?: string;
+}
